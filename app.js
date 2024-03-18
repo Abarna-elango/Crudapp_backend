8152718497ba1104072602bf7babb46bc44127c1
Abarna-elango <abarnaelango11@gmail.com>
2024-03-18 12:42:28 +0530

    connected to mongoDB database

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 import express from "express";
+import connectDB from "./database/db.js";
 const app = express();
 const PORT = 2000;
-app.listen(PORT,()=>{
-    console.log(`server running on the port http:\\localhost:${PORT}`)
-})
+
 app.get("/",(req,res)=>{
     res.send("server started!@")
 })
+
+
+const startServer= async () => {
+    try {
+      await 
+      connectDB();
+      app.listen(PORT,()=>{
+          console.log(`server running on the port http:\\localhost:${PORT}`)
+      })
+    }
+    catch(error){
+      console.log(`Error connecting to mongoDB Database ${error.message}`);
+    }
+}
+startServer();
---
+++ database/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const databaseURI = process.env.MONGODB_URI;
+const connectDB = async () =>{
+    try{
+    await mongoose.connect(databaseURI);
+    console.log (`connected to mangoDB database`)
+    }catch (error){
+        console.log(`failed to connect to mangoDB database ${error.message}`)
+        process.exit(1);
+    }
+}
+export default connectDB;
