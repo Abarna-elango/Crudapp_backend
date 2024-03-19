b5ac0eac2b1e59254ca868461318bd67897ddb45
Abarna-elango <abarnaelango11@gmail.com>
2024-03-19 12:13:12 +0530

    code completed

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,15 +1,15 @@
 import userModel from "../Model/userModel.js";
 
 // 1. add user Router
-export const addUser =async (req, res) => {
+export const addUser = async (req, res) => {
   try {
-    const {name,email,district,state} = req.body;
+    const { name, email, district, state } = req.body;
     const newUser = new userModel({
-        name,
-        email,
-        district,
-        state,
-    })
+      name,
+      email,
+      district,
+      state,
+    });
     await newUser.save();
     console.log(req.body);
 
@@ -17,24 +17,49 @@ export const addUser =async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
- 
 };
 // 2. get user Router
 //     get all user
-export const getAllUser = (req, res) => {
-  res.status(200).json({ message: "getAll user Router" });
+export const getAllUser = async (req, res) => {
+  try {
+    const AllUser = await userModel.find();
+    res.status(200).json(AllUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 // or get single user
-export const getbyIdUser = (req, res) => {
-  res.status(200).json({ message: "getbyId user Router" });
+export const getbyIdUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await userModel.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 // 3. update user Router
-export const updateUser = (req, res) => {
-  res.status(200).json({ message: "update user Router" });
+export const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = req.body;
+    const updateduser = await userModel.findByIdAndUpdate(id, user, {
+      new: true,
+    });
+    res.status(200).json(updateduser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 // 4. delete user Router
-export const deleteUser = (req, res) => {
-  res.status(200).json({ message: "delete user Router" });
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await userModel.findByIdAndDelete(id);
+    res.status(200).json({ message: "user deleted succussfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
